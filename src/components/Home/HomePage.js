d82573312106d104e205731ddf41affdca642714
kumkumsingh <kumkumsingh.r@gmail.com>
2020-06-05 22:49:00 +0200

    added lazy loading code splitting

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,17 +1,22 @@
 import './HomePage.css'
-import React from 'react'
-import { Header , Footer , WorldTotal , CountriesContainer  , ErrorBoundary} from '../index.js'
+import React , { Suspense }from 'react'
+import { Header , Footer , ErrorBoundary} from '../index.js'
+
+const WorldTotal = React.lazy(() => import('../WorldTotal/WorldTotalContainer'));
+const CountriesContainer = React.lazy(() => import('../Countries/CountriesContainer'));
 
 export default function HomePage() {
     return (
         <React.Fragment>
               <Header/> 
+              <Suspense fallback={<h2>Loading...</h2>}>
               <ErrorBoundary>
               <WorldTotal/>
               </ErrorBoundary>
               <ErrorBoundary>
               <CountriesContainer/> 
               </ErrorBoundary>
+              </Suspense>
               <Footer/>   
         </React.Fragment>
     )
